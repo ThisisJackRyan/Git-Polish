2a1a0c1708ab7746ce5c1ddaa31ed84ac552740b
Matthew Wilson <matthewjcw@ksu.edu>
2025-10-18 11:20:58 -0500

    able to access private repos

--- src/app/services/firebase.js
+++ src/app/services/firebase.js
@@ -12,12 +12,16 @@ const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 const provider = new GithubAuthProvider();
 
+provider.addScope("repo");
+provider.addScope("read:user");
+
 export const signInWithGitHub = async () => {
   try {
     const result = await signInWithPopup(auth, provider);
     const credential = GithubAuthProvider.credentialFromResult(result);
     const token = credential.accessToken;
     const user = result.user;
+    console.log(credential.accessToken);
     return { user, token };
   } catch (err) {
     console.error("GitHub login error:", err);
